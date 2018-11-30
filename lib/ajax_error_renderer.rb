c29f24dd0574970ab102d76d511ffcc0665cc7e8
willnet <netwillnet@gmail.com>
2018-11-30 10:32:27 +0900

    Return 422 by default

--- lib/ajax_error_renderer.rb
+++ lib/ajax_error_renderer.rb
@@ -1,9 +1,9 @@
 require "ajax_error_renderer/engine"
 
 module AjaxErrorRenderer
-  def render_ajax_error(location: '#error', model:)
+  def render_ajax_error(location: '#error', model:, status: 422)
     @location = location
     @model = model
-    render 'ajax_errors/error', formats: :js
+    render 'ajax_errors/error', formats: :js, status: status
   end
 end
