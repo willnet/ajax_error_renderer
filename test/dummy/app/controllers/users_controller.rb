0ca5d9b40660d9e9ddeaec730ca291783144059d
willnet <netwillnet@gmail.com>
2019-05-06 22:26:08 +0900

    :lipstick:

--- test/dummy/app/controllers/users_controller.rb
+++ test/dummy/app/controllers/users_controller.rb
@@ -39,11 +39,12 @@ class UsersController < ApplicationController
   end
 
   private
-    def set_user
-      @user = User.find(params[:id])
-    end
 
-    def user_params
-      params.require(:user).permit(:name)
-    end
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name)
+  end
 end
