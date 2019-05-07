266448ff7f2735cd7beca49058896ec7d4ac70fe
willnet <netwillnet@gmail.com>
2019-05-07 15:45:03 +0900

    Wait ajax response with sleep 1

--- test/dummy/test/system/move_test.rb
+++ test/dummy/test/system/move_test.rb
@@ -4,11 +4,12 @@ class MoveTest < ApplicationSystemTestCase
   test "if move option is true, scroll up on validation error, if false not scroll up" do
     visit new_long_and_not_move_path
     click_on "Create User"
+    sleep 1
     y_without_move = evaluate_script('window.scrollY')
 
     visit new_long_and_move_path
     click_on "Create User"
-
+    sleep 1
     y_with_move = evaluate_script('window.scrollY')
     assert(y_without_move > y_with_move)
   end
